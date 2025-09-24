1a6ccb92ec18f80c8048a44689c434fb4729962e
Vaishnavi262001 <164137331+Vaishnavi262001@users.noreply.github.com>
2025-09-24 22:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	console.log(mincost([4, 3, 2, 6]));  // 29
+console.log(mincost([1, 2, 3, 4, 5]));  // 33
+console.log(mincost([5]));  // 0
+
   
 }
 
